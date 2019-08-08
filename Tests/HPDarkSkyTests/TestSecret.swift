ddb80d377bca1cb41ae60f47cade66b6ed506a70
Henrik Panhans <henrik@panhans.dev>
2019-08-08 12:45:04 +0200

    Taking steps to keep secret out of CLI 4.0

---
+++ Tests/HPDarkSkyTests/TestSecret.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Henrik Panhans on 08.08.19.
+//
+
+import Foundation
+
+struct TestSecret {
+    static let secret = ProcessInfo.processInfo.environment["TEST_SECRET"]
+}
+nan
