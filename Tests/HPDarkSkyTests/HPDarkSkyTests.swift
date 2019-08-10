a893d77d4c230d390a0f7e3b4990e5c873a90b4c
Henrik Panhans <henrik@panhans.dev>
2019-08-10 21:33:57 +0200

    I'm very good at commit messages

--- Tests/HPDarkSkyTests/HPDarkSkyTests.swift
+++ Tests/HPDarkSkyTests/HPDarkSkyTests.swift
@@ -16,16 +16,16 @@ final class HPDarkSkyTests: XCTestCase {
     }
 
     func testSecretExistsInEnvironment() {
-        XCTAssertNotNil(TestSecret.secret, "Secret was apparently not set as env variable")
+        XCTAssertNotNil(TestSecret.secret, "Secret was not set as env variable")
     }
 
     func testCrazyLocation() {
         let crazyLocation = CLLocationCoordinate2D(latitude: 200, longitude: 300)
         let anotherCrazyLocaiton = CLLocationCoordinate2D.validated(latitude: 200, longitude: 300)
         let goodLocation = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
-        XCTAssertFalse(crazyLocation.isValidLocation)
+        XCTAssertFalse(crazyLocation.isValidLocation, "Location was falsely evaluated as valid")
         XCTAssertNil(anotherCrazyLocaiton, "Should not init with invalid coordinates")
-        XCTAssertTrue(goodLocation.isValidLocation)
+        XCTAssertTrue(goodLocation.isValidLocation, "Location was falsely evaluated as invalid")
     }
 
     func testExcludingAll() {
@@ -40,7 +40,7 @@ final class HPDarkSkyTests: XCTestCase {
                 exp.fulfill()
                 return
             }
-            //XCTAssertNil(forecast.currently, "Current weather not excluded")
+            XCTAssertNil(forecast.currently, "Current weather not excluded")
             XCTAssertNil(forecast.minutely, "Minutely forecast not excluded")
             XCTAssertNil(forecast.hourly, "Hourly forecast not excluded")
             XCTAssertNil(forecast.daily, "Daily forecast not excluded")
@@ -62,7 +62,7 @@ final class HPDarkSkyTests: XCTestCase {
                 exp.fulfill()
                 return
             }
-            //XCTAssertNotNil(forecast.currently, "Current weather is missing")
+            XCTAssertNotNil(forecast.currently, "Current weather is missing")
             XCTAssertNotNil(forecast.minutely, "Minutely forecast is missing")
             XCTAssertNotNil(forecast.hourly, "Hourly forecast is missing")
             XCTAssertNotNil(forecast.daily, "Daily forecast is missing")
